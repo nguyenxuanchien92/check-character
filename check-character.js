4107e690c54cba3152ef1f724938388f16519fda
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-10-20 10:30:08 +0700

    add condition for ex 9

--- check-character.js
+++ check-character.js
@@ -1,12 +1,18 @@
 let str = "ádasdjahdkjashdnalkdj";
 
 function checkCharacter(letter) {
-    let count = 0;
-    // creating regex
-    let re = new RegExp(letter, 'g');
 
-    // matching the pattern
-    count = str.match(re).length;
+    let count = 0;
+    if (!str.includes(letter)) {
+        return -1
+    } else {
+        let re = new RegExp(letter, 'g');
+        let result = str.match(re);
+        count = result.length;
+    }
+
+
+    // count = str.match(re).length;
 
     return count;
 }
